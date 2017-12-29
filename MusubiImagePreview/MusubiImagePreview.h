2be4a4aaf5aac90022a327399e5da0a5399d15d2
ha1f <ha1f@linecorp.com>
2017-12-29 10:35:27 +0900

    Initial Commit

---
+++ MusubiImagePreview/MusubiImagePreview.h
@@ -0,0 +1,19 @@
+//
+//  MusubiImagePreview.h
+//  MusubiImagePreview
+//
+//  Created by ST20591 on 2017/12/29.
+//  Copyright © 2017年 ha1f. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MusubiImagePreview.
+FOUNDATION_EXPORT double MusubiImagePreviewVersionNumber;
+
+//! Project version string for MusubiImagePreview.
+FOUNDATION_EXPORT const unsigned char MusubiImagePreviewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MusubiImagePreview/PublicHeader.h>
+
+
